9481461456c9d5f5e373f877f0831e61b5a56d7e
Nelson Wu <wu_nelson@outlook.com>
2017-04-05 13:47:56 +0900

    first commit

---
+++ src/main/scala/me/nelsonwu/App.scala
@@ -0,0 +1,9 @@
+package me.nelsonwu
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/me/nelsonwu/AppTest.scala
@@ -0,0 +1,17 @@
+package me.nelsonwu
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
